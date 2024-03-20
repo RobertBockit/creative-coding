cd09447b6bce2b8dcabb5b267bd7663dbb207691
CodeFanaticLV <robertterekhovich@gmail.com>
2024-03-20 19:41:17 +0200

    initializing project

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
+
+
+$(window).load(function(){
+    const canvasElement = document.getElementById('canvas1');
+    const context = canvasElement.getContext('2d');
+    canvasElement.addEventListener('mousemove', function(e) {
+        let mousePos = getMousePosition(e, canvasElement);
+        redrawCanvas(mousePos);
+    }, false);
+    function redrawCanvas(mousePos) {
+        screenWidth = window.innerWidth;
+        screenHeight = window.innerHeight;
+        console.log('a');
+        canvasElement.width = canvasElement.width;
+        context.globalAlpha = 1;
+        context.drawImage(image, 0, 0, screenWidth, screenHeight);
+        context.beginPath();
+        context.rect(0,0,screenWidth,screenHeight);
+        context.arc(mousePos.x, mousePos.y, 150, 0, Math.PI*2, true)
+        context.clip();
+        context.fillRect(0,0,screenWidth,screenHeight);
+    }
+
+
+
+    const image = new Image();
+    image.onload = function() {
+        redrawCanvas({x: -screenWidth, y: -screenHeight})
+    }
+    image.src = 'https://images.pexels.com/photos/162389/lost-places-old-decay-ruin-162389.jpeg';
+
+
+    function getMousePosition(e, canvas) {
+        let element = canvas,
+            offsetX = 0,
+            offsetY = 0,
+            mx, my;
+        if (element.offsetParent !== undefined) {
+            do {
+                offsetX += element.offsetLeft;
+                offsetY += element.offsetTop;
+            } while ((element = element.offsetParent));
+        }
+        mx = e.pageX - offsetX;
+        my = e.pageY - offsetY;
+        return {
+            x: mx,
+            y: my
+        };
+    }
+});
